305b3e2917263b1bc9e3579e3f9f3907d616f43c
Chris Simpkins <git.simpkins@gmail.com>
2021-05-14 00:18:11 -0400

    [size] add _calc_table_sizes method

--- lib/fontsize/size.py
+++ lib/fontsize/size.py
@@ -7,20 +7,21 @@ from fontTools.ttLib import TTFont
 
 class FontSize(object):
     def __init__(self, filepath):
-        self.total_bytes = 0
-        self.encoded_glyphs = 0
+        self.ttfont = TTFont(filepath)
+        self.filepath = filepath
         self.total_glyphs = 0
+        self.encoded_glyphs = 0
+        self.total_bytes = 0
         self.per_enc_glyph_bytes = 0.0
         self.per_total_glyph_bytes = 0.0
         self.tables = {}
-        self.filepath = filepath
-        self.ttfont = TTFont(filepath)
         self._calculate_sizes()
 
     def _calculate_sizes(self):
         self.total_bytes = os.path.getsize(self.filepath)
         self._calc_per_encoded_glyph_size()
         self._calc_per_total_glyph_size()
+        self._calc_table_sizes()
 
     def _calc_per_encoded_glyph_size(self):
         self.encoded_glyphs = len(self.ttfont.getBestCmap())
@@ -29,6 +30,11 @@ class FontSize(object):
     def _calc_per_total_glyph_size(self):
         self.total_glyphs = len(self.ttfont.getGlyphSet())
         self.per_total_glyph_bytes = self.total_bytes / self.total_glyphs
+
+    def _calc_table_sizes(self):
+        tables = self.get_table_tags()
+        for table in tables:
+            self.tables[table] = len(self.ttfont.getTableData(table))
 
     def get_table_tags(self):
         if self.ttfont.reader:
